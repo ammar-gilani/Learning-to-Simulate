4f802e47bcdc3f2da06540189dde4ded5df03055
ammar-gilani <gilani@u.northwestern.edu>
2019-12-30 15:09:55 -0600

    Implemented generate_valid_set().

--- Toy_Experiments.py
+++ Toy_Experiments.py
@@ -1,4 +1,6 @@
 import Config
+from sklearn.datasets.samples_generator import make_blobs
+import matplotlib.pyplot as plt
 
 print('Hi!')
 
@@ -30,7 +32,14 @@ def compute_accs(my_models, my_validation_set):
 
 # TODO
 def generate_valid_set():
-    pass
+    X = []
+    for i in range(Config.num_classes_valid):
+        X_tmp, _ = make_blobs(n_samples=Config.num_samples_validation[i], n_features=2, centers=Config.components_means[i],
+                              cluster_std=Config.components_stds[i], random_state=0)
+        X.append(X_tmp)
+        plt.scatter(X_tmp[:, 0], X_tmp[:, 1], s=40, cmap='viridis', c=Config.colors_valid[i])
+    plt.show()
+    return X
 
 
 # TODO
@@ -55,3 +64,6 @@ def learn_to_simulate():
         R = compute_accs(models, validation_set)
         A = compute_advantage_estimates(R)
         w = update_policy_pars(A)
+
+
+generate_valid_set()
