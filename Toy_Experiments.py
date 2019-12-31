09d89c921b0df0a00b6cc4fd8b83da6533c7e3f1
ammar-gilani <gilani@u.northwestern.edu>
2019-12-30 23:26:29 -0600

    Added and implemented generate_single_dataset().

--- Toy_Experiments.py
+++ Toy_Experiments.py
@@ -15,6 +15,21 @@ def generate_K_datasets(my_simulation_pars):
     return None
 
 
+def generate_single_dataset(my_means, my_stds):
+    X = []
+    for i in range(Config.num_classes):
+        X_tmp, _ = make_blobs(n_samples=Config.num_samples_training, n_features=2,
+                              centers=my_means[i],
+                              cluster_std=my_stds[i], random_state=0)
+        X.append(X_tmp)
+        if not Config.silent_mode:
+            plt.scatter(X_tmp[:, 0], X_tmp[:, 1], s=40, cmap='viridis', c=Config.colors[i])
+    if not Config.silent_mode:
+        plt.show()
+
+    return X
+
+
 # TODO
 def initialize_models():
     pass
@@ -30,15 +45,17 @@ def compute_accs(my_models, my_validation_set):
     pass
 
 
-# TODO
 def generate_valid_set():
     X = []
-    for i in range(Config.num_classes_valid):
-        X_tmp, _ = make_blobs(n_samples=Config.num_samples_validation[i], n_features=2, centers=Config.components_means[i],
-                              cluster_std=Config.components_stds[i], random_state=0)
+    for i in range(Config.num_classes):
+        X_tmp, _ = make_blobs(n_samples=Config.num_samples_training, n_features=2,
+                              centers=Config.components_means_valid[i],
+                              cluster_std=Config.components_stds_valid[i], random_state=0)
         X.append(X_tmp)
-        plt.scatter(X_tmp[:, 0], X_tmp[:, 1], s=40, cmap='viridis', c=Config.colors_valid[i])
-    plt.show()
+        if not Config.silent_mode:
+            plt.scatter(X_tmp[:, 0], X_tmp[:, 1], s=40, cmap='viridis', c=Config.colors[i])
+    if not Config.silent_mode:
+        plt.show()
     return X
 
 
@@ -64,6 +81,3 @@ def learn_to_simulate():
         R = compute_accs(models, validation_set)
         A = compute_advantage_estimates(R)
         w = update_policy_pars(A)
-
-
-generate_valid_set()
