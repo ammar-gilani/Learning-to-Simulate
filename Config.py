aba1f6950f50602bcadf626f6bdebf30c8772f95
ammar-gilani <gilani@u.northwestern.edu>
2020-01-18 22:01:49 -0600

    Added a sample for Config.py.

---
+++ Config.py
@@ -0,0 +1,10 @@
+num_iterations = 30000
+silent_mode = False
+K = 100
+colors = ['gray', 'red']
+num_classes = len(colors)
+num_components_validation = 3
+num_samples_validation = [500, 500]
+components_means_valid = [[[-9.5, -.5], [6.5, -6.5], [-3, 5]], [[1, 7.5], [6, 3], [-3, -6.5]]]
+components_stds_valid = [[.7, 1.5, 1.25], [.8, 2, 1.75]]
+num_samples_training = 500
